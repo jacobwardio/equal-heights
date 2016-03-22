d72c6b935436dc13d209ae3e206171d14cf444bb
Jacob Ward <jacobwarduk@users.noreply.github.com>
2016-03-22 21:07:15 +0000

    Slowly replacing functions

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-# Simple Equal Heights
-
+# Simple Equal Heights (No jQuery!)
 Originally sourced from [Equal height by Lewi Hussey](http://codepen.io/Lewitje/pen/YybQEP) and upgraded to work on multiple divs at once.
 A simple plugin that sets responsive equal heights by simply adding attributes directly to the mark up.
 
@@ -9,7 +8,6 @@ Download and open index.html to view the [demo](https://github.com/BurfieldCreat
 
 
 ## How to Use
-
 Group ANYTHING that needs an equal height by adding the attribute data-match-height=[groupName].
 It evens works on nested divs etc. Even works on images (although they get stretched a touch) to view and open index.html from the [demo](https://github.com/BurfieldCreative/equal-heights/archive/master.zip)
 
@@ -22,13 +20,10 @@ It evens works on nested divs etc. Even works on images (although they get stret
 ```
 
 ## Install
-[Download](https://github.com/BurfieldCreative/equal-heights/archive/master.zip) and copy the <code>simple-equal-heights.js</code> to a location in your site.
+[Download](https://github.com/BurfieldCreative/equal-heights/archive/master.zip) and copy the `simple-equal-heights.js` to a location in your site.
 
 In your web page:
 ```html
-<!-- include jQuery first -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
-
 <!-- include Simple Equal Heights -->
 <script src="http://path/to/your/copy/of/simple-equal-heights.min.js"></script>
 ```
@@ -38,3 +33,19 @@ To install simple-equal-heights via Bower:
 ```
 bower install simple-equal-heights
 ```
+
+## Changelog
+All notable changes to this project will be documented in this file.
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+### [Unreleased]
+#### Changed
+ - Refactored code to ECMAScript6 (ES2015) - by [jacobwarduk](https://github.com/jacobwarduk)
+
+### [0.0.6]() - 2016-03-22
+#### Changed
+ - *Refactored* code to remove jQuery dependency - by [jacobwarduk](https://github.com/jacobwarduk)
+ - *Refactored* code to be object literal notation - by [jacobwarduk](https://github.com/jacobwarduk)
+ - *Replaced* `!(key in groupHeights)` with `!(groupHeights.hasOwnProperty(key)`, as latter is 'correct' way to do it - by [jacobwarduk](https://github.com/jacobwarduk)
+ - *Removed* `"use strict";` as no longer required in ECMAScript6 (ES2015) - by [jacobwarduk](https://github.com/jacobwarduk)
+ - *Removed* un-descriptive comments - by [jacobwarduk](https://github.com/jacobwarduk)
--- js/simple-equal-heights.js
+++ js/simple-equal-heights.js
@@ -1,77 +1,116 @@
 // http://codepen.io/Lewitje/pen/YybQEP original copy from Lewi Hussay updated to work with multiple divs
-    // Equal height - by Burfield www.burfield.co.uk
-    // Example usage use data-match-height="groupName"> on anything!!!
+// Equal height - by Burfield www.burfield.co.uk
+// 2016-22-03 - Changed - Removed jQuery dependency (@jacobwarduk http://www.jacobward.co.uk)
+// Example usage use data-match-height="groupName"> on anything!!!
 
-    var matchHeight = (function () {
+var matchHeight = {
 
-        "use strict";
+  matchHeight: function() {
 
-        function matchHeight(){
-            //get all matched height attr
-            var groupName = $('[data-match-height]');
-            var groupHeights = [];
+    // var groupName = [].slice.call(document.querySelectorAll("[data-match-height]"));
+    // var groupHeights = [];
+    //
+    // groupName.forEach(function(groupName, key, groupHeights) {
+    //
+    //   var dataName = groupName;
+    //
+    //   var key = groupName.getAttribute("data-match-height");
+    //
+    //   if (!(groupHeights.hasOwnProperty(key))) {
+    //     groupHeights[key] = [];
+    //   }
+    //
+    //   dataName.style.minHeight = "auto";
+    //
+    //
+    //   dataName.each(function() {
+    //
+    //     groupHeights[key].push(dataName.outerHeight());
+    //
+    //   });
+    //
+    //   return groupHeights.key;
+    //
+    // });
 
-            // for each attr set the min height to auto this makes it responsive
-            $(groupName).each(function(){
+    // var obj = groupHeights;
+    //
+    // for (var index of obj) {
+    //   if (!obj.hasOwnProperty(index)) {
+    //     continue;
+    //   }
+    //
+    //   var minHeight = Math.max.apply(null, obj[index]);
+    //
+    //   if (window.innerWidth() > 600) {
+    //     document.querySelector("data-match-height=\"" + index + "\"").style.minHeight = minHeight;
+    //   } else {
+    //     document.querySelector("data-match-height=\"" + index + "\"").style.minHeight = "auto";
+    //   }
+    //
+    // }
 
-                var dataName = $(this);
 
-                var key = dataName.data('match-height');
+    var groupName = [].slice.call(document.querySelectorAll("[data-match-height]"));
+    var groupHeights = [];
 
-                //create an array of heights
-                if(!(key in groupHeights)){
-                    groupHeights[key] = [];
-                }
+    // for each attr set the min height to auto this makes it responsive
+    $(groupName).each(function(){
 
-                dataName.css('min-height', 'auto');
+      var dataName = $(this);
 
-                dataName.each(function() {
+      var key = dataName.data('match-height');
 
-                    groupHeights[key].push(dataName.outerHeight());
+      //create an array of heights
+      if(!(key in groupHeights)){
+        groupHeights[key] = [];
+      }
 
-                });
+      dataName.css('min-height', 'auto');
 
-                return groupHeights.key;
+      dataName.each(function() {
 
-            });
+        groupHeights[key].push(dataName.outerHeight());
 
-            var obj = groupHeights;
+      });
 
-            for (var index in obj) {
-                if (!obj.hasOwnProperty(index)) {
-                    continue;
-                }
+      return groupHeights.key;
 
-                var minHeight = Math.max.apply(null, obj[index]);
-
-                //if window is bigger then 600px set new height else set height to auto
-                if ($(window).width() > 600) {
-                    $('[data-match-height="'+index+'"]').css('min-height', minHeight);
-                } else {
-                    $('[data-match-height="'+index+'"]').css('min-height', 'auto');
-                }
-
-            }
-
-        }
-
-        function eventListeners(){
-            $(window).on('resize', function() {
-                matchHeight();
-            });
-        }
-
-        function init() {
-            eventListeners();
-            matchHeight();
-        }
-
-        return {
-            init: init
-        };
-
-    }());
-
-    $(document).ready(function() {
-        matchHeight.init();
     });
+
+    var obj = groupHeights;
+
+    for (var index in obj) {
+      if (!obj.hasOwnProperty(index)) {
+        continue;
+      }
+
+      var minHeight = Math.max.apply(null, obj[index]);
+
+      //if window is bigger then 600px set new height else set height to auto
+      if (window.innerWidth > 600) {
+        $('[data-match-height="'+index+'"]').css('min-height', minHeight);
+      } else {
+        $('[data-match-height="'+index+'"]').css('min-height', 'auto');
+      }
+
+    }
+
+  },
+
+  eventListeners: function() {
+    // window.onresize = function() {
+    //   matchHeight();
+    // });
+  },
+
+  init: function() {
+    this.eventListeners();
+    this.matchHeight();
+  }
+
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  matchHeight.init();
+});
